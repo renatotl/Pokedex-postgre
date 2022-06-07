03d7e0fb56fceea3e1b7852b3cea3a7fa1509b4b
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-06-07 18:34:45 -0300

    arquitetura do projeto

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const path = require("path");//importando uma lib do proprio express chamada path  essencial link o frontend com o backend
-const port = process.env.PORT || 3000; //esse code é pro heroko
+
 
 app.use(express.urlencoded())//navegador envia as informações pelo json e essas informçãoes vem do body pega as informações que o cliente digitou no unput e envia pelo json 
 app.set("view engine", "ejs"); //mortor engine da view é o ejs essencial
@@ -17,121 +17,8 @@ app.use(express.static(path.join(__dirname,"public")));//dizendo ao express a pa
 
 
 
-const pokedex =[//um arrey de objetos
-  { //objetos são separados por vírgula
-    id: 1,
-    nome: "Pikachu",
-    descricao: "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.",
-    tipo: "eletric" ,
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-    height: "0.4m",
-      weight: "6.0 KG",
-      category: "Mouse",
-      abilities: "Statc"
-  },
-  { 
-  id:2 ,
-  nome: "Charmander",
-    descricao: "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
-    tipo: "fire" ,
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
-    height: "0.6m",
-      weight: "8.5 KG",
-      category: "lizard",
-      abilities: "blaze"
-  },
-  { 
-    id:3 ,
-    nome: "Squirtle",
-      descricao: "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
-      tipo: "water" ,
-      imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
-      height: "0.5m",
-      weight: "9.0 KG",
-      category: "Tyni turtle",
-      abilities: "Torrent"
-    },
-]
-let message = "";
+const port = process.env.PORT || 3000; //esse code é pro heroko
 
-let pokemon = undefined;//se refere a linha de baixo
-
-//rotas
-app.get("/", (req, res) => {
-
-  setTimeout(() => {
-    message = "";
-  }, 1000);
-  
-  res.render("index", {pokedex, pokemon, message});// É MUITO IMPORTANDO COLOCAR O POKEDEX ENTRE CHAVES COMO JSON SÓ ASSIM A NOSSA RESPOSTA É ENVIADA foi esse code que consegui imprimir na tela está renderizando index na pasta index
-});
-// o add é um nome aleatório 
-app.post("/add", (req,res) =>{
-   const pokemon = req.body;// o que vem da requisição .body
-   message = `Pokemon, cadastrado com sucesso`;
-   //todo pokemon add já recebe um id automático
-   pokemon.id = pokedex.length + 1;
-   //no code abaixo que pegamos as informações e inserimos na pokedex
-   pokedex.push(pokemon);
-   res.redirect("/#cards")// retorna pra rota "/" que logo em seguida renderiza o index na linha 44 e 45
-});
-
-//rota get vai pegar o id  primeiro ele vai pra detalhes 
-app.get("/detalhes/:id", (req,res) => {
-  const id = +req.params.id;//este id tem que está igual a linha 
-//buscando pokemon por pokemon comprarando pokemon.id com o id recebido no parâetro
-pokemon =pokedex.find(pokemon => pokemon.id === id);
-res.redirect("/");
-
-
-})
-
-//pegar um pokemon por id. fazer uma atualização o put é pra pegar
-app.post("/update/:id", (req, res) =>{//:id estou enviando um parâmetro na rota update
- const id = +req.params.id -1;
- // o sinal de + no req é igual quando usado prompt
-
-
-    const newPokemon = req.body;
-    newPokemon.id = id + 1;
-
-    //achando a posição e está sendo add new pokemon
-    pokedex[id] = newPokemon;
-
-pokemon = undefined;
-
-   // não dá pra mandar ele denovo pro redirect
-   res.redirect("/#cards");
-})
-
-//criando a rota delete
-app.get("/delete/:id", (req,res) => {
-//pegando a posição do ID transformando ele em número e subtraindo 1
-const id = +req.params.id -1;
-
-
-delete pokedex[id]
-
-
-res.redirect("/#cards");
-
-})
-
-app.get("/info/:id", (req,res) => {
-const id = +req.params.id -1;// esses códigos foram exenciasi para info mostrar um pokemon
- 
-  pokemon = pokedex[id]
-  
-  res.render("index2", {pokedex, pokemon} )
- 
-
-})
-
-app.get("/retornar/:id", (req,res) => {
-  pokemon = undefined;
-  res.redirect("/") // quando retornar ele volta pra pg inicial
-
-})
 
 
 
